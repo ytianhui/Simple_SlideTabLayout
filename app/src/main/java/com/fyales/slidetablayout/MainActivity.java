2e9f303bbee284c9bd08cc3fc91e36da3974e5a1
ytianhui2004@gmail.com <yth220517>
2015-10-27 14:18:21 +0900

    Initial commit

---
+++ app/src/main/java/com/fyales/slidetablayout/MainActivity.java
@@ -0,0 +1,57 @@
+package com.fyales.slidetablayout;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import google.api.SlidingTabLayout;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TabViewPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+        SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        // slidingTabLayout.setDistributeEvenly(true); 是否填充满屏幕的宽度
+        slidingTabLayout.setViewPager(viewPager);
+
+        //自定义下划线颜色
+        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer(){
+            @Override
+            public int getIndicatorColor(int position) {
+                return Color.RED;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
